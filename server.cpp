ae0b6be1d9d59b47b756373e94cc89c61ac127a2
ainzsoup <gamraouis@gmail.com>
2023-05-30 20:34:42 +0100

    server working ?

--- server.cpp
+++ server.cpp
@@ -1,3 +1,5 @@
+#include <cstdio>
+#include <cstring>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -9,6 +11,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <iostream>
+#include <cstring>
 
 
 void broadcast (char *message, int message_length, int sender, fd_set *master, int max_socket)
@@ -71,6 +74,7 @@ int main (int ac, char **av)
 	fd_set master;
 	FD_ZERO(&master);
 	FD_SET(socket_listen, &master);
+	FD_SET(0, &master);
 	int max_socket = socket_listen;
 
 	std::cout << "waiting for connections..." << std::endl;
@@ -85,15 +89,16 @@ int main (int ac, char **av)
 			std::cerr << "select() failed. (" << errno << ")" << std::endl;
 			return 1;
 		}
-		for (int i = 1; i <= max_socket; ++i)
+		for (int i = 0; i <= max_socket; ++i)
 		{
 			if (FD_ISSET(i, &reads))
 			{
-				if (i == socket_listen)
+				if (i == socket_listen) // new connection
 				{
 					struct sockaddr_storage client_address;
 					socklen_t client_len = sizeof(client_address);
 					int socket_client = accept(socket_listen, (struct sockaddr*) &client_address, &client_len);
+
 					if (socket_client < 0) 
 					{
 						std::cerr << "accept() failed. (" << errno << ")" << std::endl;
@@ -106,7 +111,19 @@ int main (int ac, char **av)
 					getnameinfo((struct sockaddr*)&client_address, client_len, address_buffer, sizeof(address_buffer), 0, 0, NI_NUMERICHOST);
 					std::cout << "new connection from " << address_buffer << std::endl;
 				}
-				else
+				else if (i == 0) // stdin
+				{
+					char message[1024];
+					std::cin.getline(message, 1024);
+					strcat(message, "\n");
+					for (int j = 1; j <= max_socket; ++j)
+					{
+						if (FD_ISSET(j, &writes))
+							if (j != socket_listen)
+								send(j, message, strlen(message), 0);
+					}
+				}
+				else // socket_client
 				{
 					char message[1024];
 					int bytes_received = recv(i, message, 1024, 0);
@@ -116,40 +133,18 @@ int main (int ac, char **av)
 						close(i);
 						continue;
 					}
-					broadcast(message, bytes_received, i, &master, max_socket);
+					std::cout << "recv: " << message ;
 					for (int j = 1; j <= max_socket; ++j)
 					{
 						if (FD_ISSET(j, &master))
 						{
-							if (j == socket_listen || j == i)
-								continue;
-							else
+							if (j != socket_listen && j != i)
 								send(j, message, bytes_received, 0);
 						}
 					}
 				}
 			}
 		}
-		// for (int i = 1; i <= max_socket; ++i)
-		// {
-		// 	if (FD_ISSET(i, &writes))
-		// 	{
-		// 		if (i == socket_listen)
-		// 			continue;
-		// 		else
-		// 		{
-		// 			char message[1024];
-		// 			std::cin.getline(message, 1024);
-		// 			int bytes_sent = send(i, message, strlen(message), 0);
-		// 			if (bytes_sent < 1)
-		// 			{
-		// 				FD_CLR(i, &master);
-		// 				close(i);
-		// 				continue;
-		// 			}
-		// 		}
-		// 	}
-		// }
 	}
 	std::cout << "closing listening socket..." << std::endl;
 	close(socket_listen);
